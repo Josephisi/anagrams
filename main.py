81502bce61cdeed9b896bd43647b2697bfb1c88d
josephisi <josephisi112@gmail.com>
2022-05-28 17:15:05 +0100

    anagrams

---
+++ main.py
@@ -0,0 +1,18 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(str1, str2):
+    # [assignment] Add your code here
+        if(sorted(str1) == sorted(str2)):
+            return True
+        else:
+            return False
+
+
+str1 = "flow"
+str2 = "wolf"
+    
+print(find_anagram(str1, str2))
